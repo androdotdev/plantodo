d0146f0b64dce6cc0c3ca7f503ac95dfe47fd51d
androdotdev <androkingdom1@example.com>
2026-07-01 13:41:12 +0530

    fix: added baseurl in better-auth-clien

--- web/lib/auth-client.ts
+++ web/lib/auth-client.ts
@@ -2,5 +2,6 @@ import { createAuthClient } from "better-auth/react" // make sure to import from
 import { adminClient } from "better-auth/client/plugins"
 
 export const authClient =  createAuthClient({
+  baseURL: process.env.BETTER_AUTH_URL,
   plugins: [adminClient()]
 })
